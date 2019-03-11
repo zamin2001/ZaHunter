8508b0ba744938338a9631dadd61ea687baa0738
zamin ahmed <zahmed@zamins-macbook-air.local>
2019-03-11 11:42:50 -0500

    Zoom to current location

--- ZaHunter/ViewController.swift
+++ ZaHunter/ViewController.swift
@@ -10,15 +10,33 @@ import UIKit
 import MapKit
 import CoreLocation
 
-class ViewController: UIViewController {
+//Delegate means to report back to the view controller, in this case the CLLocation is reporting back or delegating to the ViewController/
+
+
+class ViewController: UIViewController, CLLocationManagerDelegate {
     
     @IBOutlet weak var mapview: MKMapView!
     let locationmanager = CLLocationManager()
-    
+    var region = MKCoordinateRegion()
     override func viewDidLoad() {
         super.viewDidLoad()
         locationmanager.requestWhenInUseAuthorization()
         mapview.showsUserLocation = true
+        locationmanager.delegate = self
+        locationmanager.startUpdatingLocation()
+        //Delegate allows access to new functions...usch as updating loction//
+        
     }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation])
+    {
+        let location = locations.first!
+        let center = CLLocationCoordinate2DMake(location.coordinate.latitude,location.coordinate.longitude)
+        
+        let span = MKCoordinateSpan(latitudeDelta: 0.025, longitudeDelta: 0.025)
+        region = MKCoordinateRegion(center:center, span: span)
+        mapview.setRegion(region, animated:true)
+    }
+    
     
 }
