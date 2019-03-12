8fe0050da016470c2c97767da5857f93ea48cf8b
zamin ahmed <zahmed@zamins-macbook-air.local>
2019-03-12 11:37:53 -0500

    Added information buttons to annotations

--- ZaHunter/ViewController.swift
+++ ZaHunter/ViewController.swift
@@ -58,6 +58,22 @@ class ViewController: UIViewController, CLLocationManagerDelegate,MKMapViewDeleg
         }
         
     }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        if annotation is MKUserLocation {
+            return nil
+        }
+        var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: "pin")
+        if pinView == nil {
+            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: "pinView")
+            pinView?.canShowCallout = true
+            pinView?.rightCalloutAccessoryView = UIButton(type:.infoLight)
+        }
+        else {
+            pinView?.annotation = annotation
+        }
+        return pinView
+    }
 }
 
 
